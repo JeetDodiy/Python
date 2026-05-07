a0969a4e8339c1bc6f1c1b3ae5ed80bb6c176328
JeetDodiy <jeetdodiya660741@gmail.com>
2026-05-07 11:18:54 +0530

    add py logical opraters

--- frist.py
+++ frist.py
@@ -1,1 +1,57 @@
-print ("hii my name is jeet")
+# Printing String 
+# print ("hii jeet")
+
+# Printing Integer  and Check the Type
+# a = 120
+# print(a)
+# print(type(a))
+
+# Printing Charecter and ASCII Value
+#  b = 'E'
+#  print(ord(b))
+
+# Printing Charecter using Index
+#  c = 'hii jeet'
+#  print(c[5])
+
+# syasing String index
+# d = 'This is jeet Python tutorial'
+# print(d[3:9])
+
+# Type Conversion
+# f = 120.54
+# print(type(f))
+ 
+# f = str(f)
+# print(type(f))
+
+# Getting Input from User
+#  name = input("Input your name:")
+#  print("this is my name: ",name)
+
+# Formate ting String
+#  name = str(input("Input your name:"))
+#  age = int(input("Input your age:"))
+#  print(f"This is my name :- {name} and this my age :-{age}")
+
+# Arethmetic Opretors ( + , - , * , /, %, // , ** )
+
+a = 10 
+b = 5
+
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a / b)
+
+#For fllow divition use (//)
+# print(a // b)
+
+#For power use (**) 
+# print(a ** b)
+
+#Modulo use (%)
+# print(a % b)
+
+#Bod mass law ( )
+print(4 + 25 / 5 * 4 - 3)
