d22c1bbf9abb2cce9c95b5eb55f1ac24c239834c
JeetDodiy <jeetdodiya660741@gmail.com>
2026-05-09 09:12:14 +0530

    add for loop with break , continue and else statment

---
+++ Learnig PY/Day3.py
@@ -0,0 +1,31 @@
+# for loop with break , continue and else
+
+#----------------------------------------------------------------
+#                  break  
+# for i in range(1,11):
+#     if i == 7:
+#         break
+#     print(i)
+
+#----------------------------------------------------------------
+#                 Continu
+# for i in range(1,16):
+#     if i == 11:
+#         continue
+#     print(i)
+
+#----------------------------------------------------------------
+
+#                 else
+#in python else statment all so use with for loop where the break statment not in use 
+# the else statmet auto maticly called 
+
+# for i in range(1,21):
+#     if i == 22:
+#         print('break in use')
+#         break
+#     print(i)
+# else:
+#     print("Break no in use")
+
+#----------------------------------------------------------------
