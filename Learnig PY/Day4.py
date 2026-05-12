906b301369d510f396008eefcb6ea91fc243f7a0
JeetDodiy <jeetdodiya660741@gmail.com>
2026-05-12 10:02:13 +0530

    Add python while loop

---
+++ Learnig PY/Day4.py
@@ -0,0 +1,52 @@
+#While loop Stamant 
+# a = 1
+# while a <= 10:
+#     print(a)
+#     a = a + 1
+
+# a = 567
+# while a > 0:
+#     print(a % 10)
+#     a = a //10
+
+#print Revars value
+# a = int(input('enter number :-'))
+# rev = 0
+# while a > 0:
+#     rev = rev * 10  + a % 10
+#     a = a //10
+# print(rev)
+
+#check pelibrom number 
+# a = int(input('Enter number for check Pelinbrom number :-'))
+# b = a
+# rev = 0
+# while a > 0:
+#     rev = rev * 10 + a % 10
+#     a = a // 10
+# if rev == b:
+#     print(f'your number is pelindrom :{b} : {rev}')
+# else:
+#     print(f'your number is not pelindrom :{b} : {rev}')
+
+#Choos random number
+# import random
+# import os
+# gess = int(input("Enter randum gess number beetwen 1 to 10:"))
+# rean = random.randint(1,10)
+# while True:
+#     if gess == rean:
+#         print("your are write ")
+#         break
+#     else:
+#         print("GAME OVER!")
+#         # Safe prank effect
+#     os.system("cls")  # clears screen on Windows
+    
+#     print("""
+#     *********************************
+#     *   SYSTEM FAILURE DETECTED!    *
+#     *********************************
+#     """)
+    
+    
