ab7aa3a22322f39a90684c764c33b584c99ea44d
JeetDodiy <jeetdodiya660741@gmail.com>
2026-05-13 11:48:58 +0530

    Day 5 is short good by

---
+++ Learnig PY/Day5.py
@@ -0,0 +1,67 @@
+#List
+# a = [1,5,8,2]
+# print(a)
+
+#(append) add singel item
+# a.append(45)
+# a.append('jeet')
+# print(a)
+
+#Insert (add Item index place,Item Value) add item
+# a.insert(2,'Dodiya')
+# print(a)
+
+#extand add maltipal Vlaue in end
+# a.extend(['dodiya',2,'jeet'])
+# print(a)
+
+#Remove funcation use to remove value in list using index number
+# a.remove(1)
+# print(a)
+
+#remove one list and add another varibale  
+# popped_item = a.pop(2)
+# print(a)
+# print(popped_item)
+
+#Find the index numberr of list using Index function
+# index = a.index(3)
+# print(a)
+# print(index)
+
+#check the all the list and count given value in list
+# caunt = a.count(1)
+# print(a)
+# print(caunt)
+
+#sort the list in the asending oder
+# a.sort()
+# print(a)
+
+#Reverse the list
+# a.reverse()
+# print(a)
+
+#copy the list
+# copy_list = a.copy()
+# print(a)
+# print(copy_list)
+
+#clear the all the list
+# a.clear()
+
+# pone = [1,5,8,-1,-3,0,2,9,-20]
+# print(pone)
+# pone.sort()
+# print(pone)
+
+#Finde the largest value
+# value = [325,12,45,875,4,1235,12,-4,7,-5,78,-45]
+# larg = value[0]
+# inde = 0
+
+# for i in range(len(value)):
+#     if value[i] > larg:
+#         larg = value[i]
+#         inde = i
+# print(f'Largest value in list :{larg} and his index is :{inde}')
