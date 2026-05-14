7af0caab52a9fb5264bb300b094337d12c51a088
JeetDodiy <jeetdodiya660741@gmail.com>
2026-05-14 08:57:38 +0530

    add tuple

--- Learnig PY/Day6.py
+++ Learnig PY/Day6.py
@@ -1,1 +1,37 @@
- 
+#check secand largest number
+# a = [12,45,78,-7,-85,125,458,121,421]
+
+# largest = a[0]
+# secand_largest = a[0]
+
+# for i in a:
+#     if i > largest:
+#         secand_largest = largest
+#         largest = i
+#     elif i > secand_largest:
+#         secand_largest = i
+# print(f'the largest number was :{largest} and secan largest number was :{secand_largest}')
+
+#check the list was sort or not
+
+# a = [-7,8,21,145,1254,1255]
+# for i in range(len(a)-1):
+#     if a[i] < a[i+1]:
+#         continue
+#     else:
+#         print("ypur list was not sorted")
+#         break
+# else:
+#     print('your list was shorted')
+
+#TUPLES 
+# a = (1,2,4,5,6,6,7,8,8)
+
+# print(type(a))
+
+# indexxing = a.index(5)
+# print(indexxing)
+
+# counte = a.count(8)
+# print(counte)
+
