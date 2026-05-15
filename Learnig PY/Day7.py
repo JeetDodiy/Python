bf4f6be25880db2a54fe1d5dc45f10a430853e36
JeetDodiy <jeetdodiya660741@gmail.com>
2026-05-15 10:26:33 +0530

    add sumset opration and methods

---
+++ Learnig PY/Day7.py
@@ -0,0 +1,78 @@
+#set
+s = {-12,1,2,3,5,2,1,4,8,7}
+# print(s)
+# print(type(s))
+
+# for i in s:
+#     print(i)
+
+# s.add(6)
+# print(s)
+
+# s.remove(6)
+# print(s)
+
+# s.discard(7)
+# print(s)
+
+# print(s)
+# pop_eli = s.pop()
+
+# print(pop_eli)
+
+# s.clear()
+# print(s)
+
+
+#sume set oprection
+#murge two set 
+a = {1,2,3,4,5}
+b = {4,5,6,7,8,9}
+
+# c =a.union(b)
+# print(c)
+
+# c = a|b
+# print(c)
+
+#murge two set and check coman value
+
+# c = a.intersection(b)
+# print(c)
+
+# c = a&b
+# print(c)
+
+
+# check two set and get difrence beetwin
+
+# c = a.difference(b)
+# print(c)
+
+# c = b.difference(a)
+# print(c)
+
+# c = a-b
+# print(c)
+
+# c = b-a
+# print(c)
+
+
+#remove same value show only non repited value
+
+# c = a.symmetric_difference(b)
+# print(c)
+
+# c = a^b
+# print(c)
+
+# most IMP you can use also compaund oprection also
+# a |= b
+# a &=b
+# a -= b
+# a ^= b
+
+# print(a)
+
+
