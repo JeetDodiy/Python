110519d4d5782da0fd71e61cfdb670876283f57b
JeetDodiy <jeetdodiya660741@gmail.com>
2026-05-18 11:05:19 +0530

    Complit Excaption handling

---
+++ Learnig PY/Day9.py
@@ -0,0 +1,35 @@
+# the error not acurs else block run automaticly
+# a =10
+# b = int(input("Enter number :"))
+# try:
+#     print(a/b)
+# except Exception as err:
+#     print(f"you have error :{err}")
+# else:
+#     print("good Boy")
+# print("vary good")
+
+#the Finlly block all ways run no mmater what. the try block throw the error and except the error 
+# a =10
+# b = int(input("Enter number :"))
+# try:
+#     print(a/b)
+# except Exception as err:
+#     print(f"you have error :{err}")
+# else:
+#     print("good Boy")
+# finally:
+#     print("I run no mater what")
+# print("vary good")
+
+# raise Error 
+# we create on Exption Error using raise function
+# try:
+#     age = int(input("Eter your Age :"))
+#     if age < 10 or age > 18:
+#         raise ValueError("you not in in boys grup")
+#     else:
+#         print("yaa boy in the grup")
+# except Exception as err:
+#     print(f"boy Error aavel he:{err}")
+# print("OHO Boy grup not exist")
