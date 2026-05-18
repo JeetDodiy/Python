b038b195228b08bf4801bfc18bbcc38049e3978d
JeetDodiy <jeetdodiya660741@gmail.com>
2026-05-18 12:01:54 +0530

    Complit the file handling

--- Learnig PY/Day9.py
+++ Learnig PY/Day9.py
@@ -33,3 +33,39 @@
 # except Exception as err:
 #     print(f"boy Error aavel he:{err}")
 # print("OHO Boy grup not exist")
+
+#---------------------File handling-----------------------------------------------
+# In python File handling have 4 type of mode 
+# (1) r = REad mode this only use to read file and the deflot mde of opne was 'r'
+# (2) w = Write mode this mode can create a new file and over write the curent file
+# (3) a = Apend mode this mode also create a new file and add new tex in olde file
+# (4) x =       mode this mode use to only create file 
+#----------------------------------------------------------------------------------
+
+# Open File [r mode]
+# p = open(r"E:\jeetPython\Learnig PY\Day1.py")
+# print(p.read())
+
+# w = open("create_file_w.tex",'w')
+# w.write("The file ceate by W mode")
+# w.close()
+
+# w = open("create_file_w.tex",'w')
+# w.write("Rewrite the fil using W mode")
+# w.close()
+
+# create file and addthe text usig the apend mode
+# a = open("create_file_a.tex",'a')
+# a.write(' halelu ua hii this')
+# a.close()
+
+# a = open("create_file_a.tex",'a')
+# a.write(' ,Add tex usinf A mode')
+# a.close()
+
+# create file using  x mode
+# x = open("create_file_x.tex",'x')
+# x.close()
+
+#----------------------------------------------------------------------------------
+
