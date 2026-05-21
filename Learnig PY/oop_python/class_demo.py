bc7369c86eed41259ecdec84b1de65f439fa8edf
JeetDodiy <jeetdodiya660741@gmail.com>
2026-05-21 10:11:34 +0530

    Add object and class

---
+++ Learnig PY/oop_python/class_demo.py
@@ -0,0 +1,8 @@
+class Demo:
+    a = 10
+    def in_name(self):
+        print("Jeet dodiya")
+    # print("Hii this is jeet") # In the class This statment print were the program was run thats way do not use this type of statment
+
+print(Demo().a)
+print(Demo().in_name())
---
+++ Learnig PY/oop_python/objeckt_demo.py
@@ -0,0 +1,9 @@
+class Object_Demo:
+    def demo(self):
+        print("hii this is Object Demo")
+    def demo2(self):
+        print("Hii this is Demo 2")
+
+aav = Object_Demo()
+aav.demo()
+aav.demo2()
