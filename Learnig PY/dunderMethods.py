a12475e3e7c9f6692d9ddd64b74c41588f155217
JeetDodiy <jeetdodiya660741@gmail.com>
2026-05-23 10:56:50 +0530

    Add dunder Methods

---
+++ Learnig PY/dunderMethods.py
@@ -0,0 +1,33 @@
+#-----------------------------------------------------------------------------------------------------------------------
+
+# the dunder Methods was created by Python to use Farst rispons and esay to use the class and object in Python
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+# class Human:
+#     def __init__(self,name,age):
+#         self.name = name
+#         self.age = age
+
+#     def  __str__(self):
+#         return f"hi this Human name was :{self.name} AND the age was :{self.age} "
+    
+#     def __add__(self, other):
+#         sum = 0
+#         for i in other:
+#             sum = sum + i.age
+#         return f"the sume of age is {self.age + sum}"
+    
+# h = Human("jeet" , 21)
+# h1 = Human("rathod" , 20)
+# h2 = Human("rakesh" , 24)
+
+# print(h)
+# print(h + (h1,h2))
+
+#-------------------------------------------------------IMP-------------------------------------------------------------
+
+# the very Difrent type of Dunder methos are in Python you can Learn the difreant Dunder methods in python
+# https://www.pythonmorsels.com/every-dunder-method/
+
+#-----------------------------------------------------------------------------------------------------------------------
