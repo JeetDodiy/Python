a770d3caab47afc347588e088320eea4bc536be5
JeetDodiy <jeetdodiya660741@gmail.com>
2026-05-23 10:16:51 +0530

    Add abstraction Method

---
+++ Learnig PY/oop_python/abstraction_demo.py
@@ -0,0 +1,35 @@
+# In the Python abstraction wad normaly not exsits but sum way to achive the abstraction 
+
+# from abc import ABC, abstractmethod
+# class Abstract(ABC):
+#     @abstractmethod
+#     def name(self):
+#         pass
+#     @abstractmethod
+#     def type(self):
+#         pass
+
+# class CarCompny(Abstract):
+#     def compnyName(self):
+#         print("Compny Name was Ford")
+
+# class BikeCompny(Abstract):
+#     def bikeName(self):
+#         print("Z900")
+#     def name(self):
+#         print("Kawasaki")
+#     def type(self):
+#         print("Super Bike")
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+# Where the abstractmethod Inherited the class complar sari inherited the perent methods where the class not 
+# use the abstract Class methods that have Error 
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+# cc = CarCompny()
+# bc = BikeCompny()
+# bc.bikeName()
+# bc.name()
+# bc.type()
