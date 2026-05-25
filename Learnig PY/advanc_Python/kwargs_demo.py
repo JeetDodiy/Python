59faba0fbde430cd2bb20eb2d22024a5932bcb52
JeetDodiy <jeetdodiya660741@gmail.com>
2026-05-25 12:58:20 +0530

    Add kwargs

---
+++ Learnig PY/advanc_Python/kwargs_demo.py
@@ -0,0 +1,8 @@
+# kwargs was get Key or Value 
+# def human(**kwargs):
+#     print("Your Information is \n")
+#     for i in kwargs:
+#         print(f"{i} : {kwargs[i]}")
+    
+
+# human(name = "Jeet" , age = "18" )
