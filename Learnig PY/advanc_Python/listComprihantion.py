570d66543d63e9395f7c24a6231bba3eac358755
JeetDodiy <jeetdodiya660741@gmail.com>
2026-05-26 15:39:19 +0530

    add Python advanction function

---
+++ Learnig PY/advanc_Python/listComprihantion.py
@@ -0,0 +1,34 @@
+# List compriihantion 
+# Where you Write the single line of code and that was get maltipal work in one line
+# a = [i for i in range(1,21) if i % 2 == 0 ]
+# print(a)
+
+# Dist compriihantion 
+# in dist {key : value}
+# l = {i : i**2 for i in range(1,11)}
+# print(l)
+
+
+# unic
+# l = {i * i for i in range(10) if i % 2 == 0}
+# print(l)
+
+# Lambda Function
+# Add  = lambda a,b : a + b
+# print(Add(12,12)) 
+
+# Map 
+# a = [2,4,6,8]
+# result = map (lambda c : c * c , a)
+# print(list(result))
+
+# filter 
+# def odd(x):
+#     if x % 2 != 0:
+#         return True
+#     else:
+#         return False
+    
+# a = [1,2,4,5,7,8,5,6,9,7,8]
+# result = filter(odd , a)
+# print(list(result))
