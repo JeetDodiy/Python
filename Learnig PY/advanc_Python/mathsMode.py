ec33d5cebbb554a101b68d562ed0a4eb3157c5a7
JeetDodiy <jeetdodiya660741@gmail.com>
2026-05-26 15:55:57 +0530

    add packej and moduals in python

---
+++ Learnig PY/advanc_Python/mathsMode.py
@@ -0,0 +1,4 @@
+def additon(a,b):
+    print(a + b)
+def malti(a,b):
+    print(a * b)
---
+++ Learnig PY/advanc_Python/modualAndPackeg.py
@@ -0,0 +1,8 @@
+# import mathsMode
+# mathsMode.additon(45,45)
+# mathsMode.malti(2,5)
+
+# In python Moduals minss The single file that cald moduals 
+# The colection of moduals cald packeg 
+
+# Like in this sereas the packeg was like advanc_pthon , oop_python theds was
